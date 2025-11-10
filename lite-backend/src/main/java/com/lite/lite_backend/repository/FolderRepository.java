416ff4abec8c4e4332a1c8dcf2f449b8c500803a
Ilyass <129749723+Ilyas-exe@users.noreply.github.com>
2025-11-10 10:54:48 +0100

    Add method to find folders by user
    Introduced findByUser(User user) to FolderRepository for retrieving all folders belonging to a specific user.

--- lite-backend/src/main/java/com/lite/lite_backend/repository/FolderRepository.java
+++ lite-backend/src/main/java/com/lite/lite_backend/repository/FolderRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 @Repository
 public interface FolderRepository extends JpaRepository<Folder, Long> {
+    List<Folder> findByUser(User user);
+
     List<Folder> findByUserAndParentFolderIsNull(User user);
 
     List<Folder> findByUserAndParentFolder(User user, Folder parentFolder);
