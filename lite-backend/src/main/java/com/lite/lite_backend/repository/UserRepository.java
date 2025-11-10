62cb260d876e39ee332fc032226e519efba8eab5
Ilyass <129749723+Ilyas-exe@users.noreply.github.com>
2025-11-10 10:54:54 +0100

    Remove unnecessary whitespace in UserRepository
    Cleaned up formatting by removing extra whitespace in the UserRepository interface.

--- lite-backend/src/main/java/com/lite/lite_backend/repository/UserRepository.java
+++ lite-backend/src/main/java/com/lite/lite_backend/repository/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+
     /**
      * Find a user by their email address.
      * This will be used for authentication and user lookup.
