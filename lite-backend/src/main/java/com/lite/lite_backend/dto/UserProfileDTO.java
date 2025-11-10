489a4b66db571997b66a1d12dbca03c147a27198
Ilyass <129749723+Ilyas-exe@users.noreply.github.com>
2025-11-10 10:54:43 +0100

    Add UserProfileDTO for user data transfer
    Introduces UserProfileDTO class with username and email fields to facilitate user profile data transfer. Utilizes Lombok annotations for boilerplate code reduction.

---
+++ lite-backend/src/main/java/com/lite/lite_backend/dto/UserProfileDTO.java
@@ -0,0 +1,13 @@
+package com.lite.lite_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfileDTO {
+    private String username;
+    private String email;
+}
